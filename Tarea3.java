2af381b96969fcfaa3728f49c939e58febee7c71
fexbard <fexbarreda@gmail.com>
2023-01-21 18:49:54 -0300

    tarea número 3

---
+++ Tarea3.java
@@ -0,0 +1,18 @@
+package com.tar3;
+
+public class Tarea3 {
+
+
+    public static void main(String[] args) {
+
+        String[] planetasRocosos = {"Mercurio", "Venus", "Tierra", "Marte"};
+        String planet;
+        for (int j =0; j < planetasRocosos.length; j++){
+//            System.out.println(planetasRocosos[j]);
+
+        }
+String planetasArray= planetasRocosos[0]+" "+ planetasRocosos[1]+" "+planetasRocosos[2]+" "+planetasRocosos[3];
+        System.out.println(planetasArray);
+}
+
+    }
